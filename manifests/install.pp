53147cc597a1653978c87af9323774dbcbb3eebc
root <root@puppetserver.rocket.local>
2021-04-27 12:47:33 -0500

    Add install class with CentOS 7 support

---
+++ manifests/install.pp
@@ -0,0 +1,10 @@
+#  Installs base Apache
+#
+class apache::install (
+  $install_name   = $apache::params::install_name,
+  $install_ensure = $apache::params::install_ensure,
+) inherits apache::params {
+  package { "${install_name}":
+    ensure => $install_ensure,
+  }
+}
