465b3a5f5c17cafe8d0951138e9254c1c01f1dde
root <root@puppetserver.rocket.local>
2021-04-27 14:36:27 -0500

    Added config class to manage configuration files

---
+++ manifests/config.pp
@@ -0,0 +1,13 @@
+# @summary A short summary of the purpose of this class
+#   Manages configuration files for Apache
+#
+class apache::config {
+  file { 'apache_config':
+    ensure => $apache::config_ensure,
+    path   => $apache::config_path,
+    source => "puppet:///modules/apache/${osfamily}.conf",
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'root',
+  }
+}
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,12 @@
-class apache {
-  include apache::install
+class apache (
+  String $install_name,
+  String $install_ensure,
+  String $config_ensure,
+  String $config_path,
+) {
+  contain apache::install
+  contain apache::config
+
+  Class['::apache::install']
+  -> Class['::apache::config']
 }
